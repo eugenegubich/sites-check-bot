cd871f60f72e27d41c698047db1ff3650a3466d8
Eugene Gubich <eugengubich@gmail.com>
2025-01-16 17:05:10 +0000

    domain expiration check

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import paramiko
 import json
 import re
+import whois
+import datetime
 
 dotenv.load_dotenv()
 
@@ -13,6 +15,17 @@ def telegram_sendfile(file_path, message, token, chat_id):
     data = {"chat_id": chat_id, "caption": message}
     resp = requests.post(url, files=files, data=data)
     return resp.json()
+
+def get_domain_expiration_date(domain):
+    try:
+        w = whois.whois(domain)
+        expiration_date = w.expiration_date
+        if expiration_date:
+            return expiration_date
+        else:
+            return "No data"
+    except Exception:
+        return "No data"
 
 def get_sites_list(server_list, ssh_private_key_file, ssh_user, ssh_port):
     key = paramiko.Ed25519Key.from_private_key_file(ssh_private_key_file)
@@ -81,8 +94,9 @@ def check_sites(sites_list, file_path):
             status_code = 0
             failed = True
         
-        current_status = {"domain": sitename, "state": state, "status_code": status_code, "server": server, "responce_time": responce_time, "failed": failed}
-        print(current_status)
+        domain_expire = get_domain_expiration_date(sitename)
+        
+        current_status = {"domain": sitename, "state": state, "status_code": status_code, "server": server, "responce_time": responce_time, "failed": failed, "domain_expire": domain_expire}
         status.append(current_status)
     with open(file_path, "w") as f:
         json.dump(status, f)
@@ -94,7 +108,7 @@ def count_server_errors(requests_status):
     for site in requests_status:
         server = site['server']
         if not any(server in d['server'] for d in servers_status):
-            servers_status.append({'server': server, 'errors': 0, 'working_sites': 0})
+            servers_status.append({'server': server, 'errors': 0, 'working_sites': 0, 'expired_domains': 0, 'no_data_domains': 0})
         if site['failed']:
             for server_status in servers_status:
                 if server_status['server'] == server:
@@ -103,9 +117,21 @@ def count_server_errors(requests_status):
             for server_status in servers_status:
                 if server_status['server'] == server:
                     server_status['working_sites'] += 1
+        if site['domain_expire'] != "No data":
+            for server_status in servers_status:
+                if server_status['server'] == server:
+                    expire_date = datetime.datetime.strptime(site['domain_expire'], "%Y-%m-%d")
+                    if expire_date < datetime.datetime.now():
+                        server_status['expired_domains'] += 1
+        else:
+            for server_status in servers_status:
+                if server_status['server'] == server:
+                    server_status['no_data_domains'] += 1
+
+                
     message = ""
     for server in servers_status:
-        message += f"Server: {server['server']}\nErrors: {server['errors']}\nWorking sites: {server['working_sites']}\n\n"
+        message += f"Server: {server['server']}\nErrors: {server['errors']}\nWorking sites: {server['working_sites']}\nExpired domains: {server['expired_domains']}\nNo data domains: {server['no_data_domains']}\n\n"
     return message
 
 
@@ -116,5 +142,6 @@ server_list = [server.strip() for server in server_list]
 sites = get_sites_list(server_list, os.getenv("SSH_PRIVATE_KEY_FILE"), os.getenv("SSH_USER"), os.getenv("SSH_PORT"))
 check_result = check_sites(sites, os.getenv("RESULT_FILE_PATH"))
 message = count_server_errors(check_result)
+print(message)
 
 
